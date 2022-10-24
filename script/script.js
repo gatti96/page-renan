eab271bacdc11548cf617d400a44818236faf774
Renan Gatti <renanfrancisco96@gmail.com>
2022-10-24 16:51:41 -0300

    contato

--- script/script.js
+++ script/script.js
@@ -27,3 +27,88 @@ const interval = setInterval(() => {
 }, 1000);
 
 //fim captura da data
+
+function paginaAtual() {
+  alert("Página atual!");
+}
+
+// abrir formulário
+
+function addForm() {
+  const contactSection = document.getElementById("form");
+
+  const div = document.createElement("div");
+  const h1 = document.createElement("h1");
+  h1.innerText = "Formulário para contato";
+
+  const div2 = document.createElement("div");
+  const form = document.createElement("form");
+  form.action = "https://formsubmit.co/renanfrancisco96@gmail.com";
+  form.method = "post";
+
+  //input name
+  const div3 = document.createElement("div");
+  div3.className = "input-group mb-3";
+  div3.innerHTML =
+    '<label for="name" class="input-group-text color">Nome:</label';
+  const nameInput = document.createElement("input");
+  nameInput.type = "text";
+  nameInput.name = "nome";
+  nameInput.className = "form-control";
+  nameInput.id = "name";
+  nameInput.placeholder = "Nome Completo";
+  div3.appendChild(nameInput);
+  //input email
+  const div4 = document.createElement("div");
+  div4.className = "input-group mb-3";
+  div4.innerHTML =
+    '<label for="email" class="input-group-text color">E-mail:</label>';
+  const emailInput = document.createElement("input");
+  emailInput.type = "email";
+  emailInput.name = "email";
+  emailInput.className = "form-control";
+  emailInput.id = "email";
+  emailInput.placeholder = "E-mail";
+  div4.appendChild(emailInput);
+  //input assunto
+  const div5 = document.createElement("div");
+  div5.className = "input-group mb-3";
+  div5.innerHTML =
+    '<label for="assunto" class="input-group-text color">Assunto:</label>';
+  const assuntoInput = document.createElement("input");
+  assuntoInput.type = "text";
+  assuntoInput.name = "assunto";
+  assuntoInput.className = "form-control";
+  assuntoInput.id = "assunto";
+  assuntoInput.placeholder = "Assunto";
+  div5.appendChild(assuntoInput);
+
+  //text area
+  const div6 = document.createElement("div");
+  div6.className = "input-group";
+  div6.innerHTML =
+    '<span class="input-group-text color">Digite sua mensagem:</span>';
+  const textArea = document.createElement("textarea");
+  textArea.name = "mensagem";
+  textArea.className = "form-contro size-text-area";
+  textArea.ariaLabel = "Digite sua mensagem";
+  textArea.placeholder = "Digite sua mensagem";
+  div6.appendChild(textArea);
+
+  const tanks = document.createElement("input");
+  tanks.type = "hidden";
+  tanks.name = "_next";
+  tanks.value = "https://yourdomain.co/thanks.html";
+
+  const div7 = document.createElement("div");
+  div7.className = "col-12 center";
+  div7.innerHTML =
+    '<button type="submit" class="btn color-submit">Enviar</button>';
+
+  //junção de todos
+  form.append(div3, div4, div5, div6, tanks, div7);
+  div2.appendChild(form);
+  div.appendChild(h1);
+
+  contactSection.append(div, div2);
+}
